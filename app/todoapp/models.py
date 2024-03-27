1b487d3f16562218b9e1c41a97515dbd4429bb6b
Talha Munir <talhamunir411@gmail.com>
2024-03-27 11:46:08 +0500

    Adjusted the model a bit to match my requirements.

--- app/todoapp/models.py
+++ app/todoapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, default="Default description")
--- app/todoapp/serializers.py
+++ app/todoapp/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields=('name', 'description')
+        fields=('id', 'name', 'description')
